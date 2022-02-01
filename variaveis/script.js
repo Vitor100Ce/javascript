a3c47dfaafb5f4ebc7670b26316a1f08d5134161
Vitor100Ce <88120850+Vitor100Ce@users.noreply.github.com>
2022-02-01 16:01:01 -0300

    primeira variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,28 @@
+// Declarar uma variável com o seu nome
+
+let nome = 'Vitor';
+console.log(nome);
+
+// Declarar uma variável com a sua idade
+
+let idade = 26;
+console.log(idade);
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+let comida;
+console.log(comida);
+
+// Atribuir valor a sua comida favorita
+
+comida = 'sushi'
+console.log(comida)
+
+// Declarar 5 variáveis diferentes sem valores
+let velocidade,
+    tamanho,
+    local,
+    cidade,
+    marca;
+    
---
+++ variaveis/scriptAula.js
@@ -0,0 +1,31 @@
+var nome = 'Vitor';
+var idade = 26;
+var possuiFaculdade = true;
+
+console.log(nome,idade,possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobreNome = 'Ramos',
+    cidade = 'Ocidental';
+
+console.log(sobreNome,cidade)
+
+/* console.log(naoDefinido) */
+
+var comida;
+console.log(comida);
+comida = 'Pizza';
+console.log(comida);
+
+
+let time = 'Flamengo';
+console.log(time);
+time = 'Vasco'
+console.log(time);
+
+
