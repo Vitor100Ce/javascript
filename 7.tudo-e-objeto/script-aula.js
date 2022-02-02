62c3de9103d3676f0b76748c0691776139eef2bf
Vitor100Ce <88120850+Vitor100Ce@users.noreply.github.com>
2022-02-02 13:37:12 -0300

    Metódos e propriedades

--- 7.tudo-e-objeto/script-aula.js
+++ 7.tudo-e-objeto/script-aula.js
@@ -1,36 +1,9 @@
-var pessoa = {
-    nome: 'Vitor',
-    idade: 26
-}
+let nome =  'Vitor';
 
-console.log(pessoa.nome)
+let nomeMinusculo = nome.toLowerCase();
 
-var quadrado = {
-    lados: 4,
-    area(lado){
-        return lado * lado;
-    },
-    perimetro(lado){
-        return this.lados * lado
-    }
-}
+let btn = document.querySelector('.btn');
 
-console.log(quadrado.area(7));
-console.log(quadrado.perimetro(9));
-
-
-console.log(Math.random());
-
-var menu = {
-    width: 800,
-    height: 50,
-    backgroundColor: '#84E',
-    metadeHeigth(){
-        return this.height / 2;
-    }
-}
-
-menu.backgroundColor = '#000';
-console.log(menu.backgroundColor);
-
-var bg = menu.backgroundColor;
+btn.addEventListener('click', function(){
+  console.log('Clicou')
+})
---
+++ 7.tudo-e-objeto/script.js
@@ -0,0 +1,26 @@
+// nomeie 3 propriedades ou métodos de strings
+
+nome = 'Vitor';
+
+nome.length;
+nome.toLocaleLowerCase();
+nome.replace('Vi', 'To');
+
+console.log(nome.replace('Vi', 'To'));
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function(){
+    console.log('Clicou');
+})
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+
+
+
